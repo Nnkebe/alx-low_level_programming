f6abf820e6150dcca1a46f1443a37f2e5ebfdfc2
Nnkebe <nnkebe@gmail.com>
2023-06-15 09:59:08 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,6 +15,6 @@ printf("size of char: %lu byte(s)\n", (unsigned long)sizeof(a));
 printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
 printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
-printf("a float: %lu byte(s)\n", (unsigned int)sizeof(f));
+printf("size of a float: %lu byte(s)\n", (unsigned int)sizeof(f));
 return (0);
 }
