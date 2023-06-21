a8b73988a1f423c4ddb8a86b68df6d6d224f01fd
Nnkebe <nnkebe@gmail.com>
2023-06-20 20:28:15 -0700

    100-times_table.c

---
+++ 0x02-functions_nested_loops/es_table.c
@@ -0,0 +1,17 @@
+_table.c: In function ‘print_times_table_table.c: In function ‘print_times_table’:
+100-times_table.c:13:14: error: expected ‘)’ before ‘;’ token
+   13 |  if ((n >= 0); &&n <= 15)
+         |     ~        ^
+	       |              )
+	       100-times_table.c:15:31: error: expected ‘)’ before ‘;’ token
+	          15 |  for (num = 0; num <= n; num++;)
+		        |      ~                        ^
+			      |                               ):
+100-times_table.c:13:14: error: expected ‘)’ before ‘;’ token
+   13 |  if ((n >= 0); &&n <= 15)
+         |     ~        ^
+	       |              )
+	       100-times_table.c:15:31: error: expected ‘)’ before ‘;’ token
+	          15 |  for (num = 0; num <= n; num++;)
+		        |      ~                        ^
+			      |                               )
